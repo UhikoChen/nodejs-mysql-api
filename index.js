bfb4d0b59ac99d69abcdadbaf5a95bd878b14714
UhikoChen <chen20825172@gmail.com>
2023-06-03 22:15:38 +0800

    Feat: 新增Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express()
 
 require('dotenv').config()
 
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
